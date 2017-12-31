9d20e8cd27bb13e34de5500f4f9bcca9194356a2
knyazsergei <yaknyazsergei@yandex.ru>
2017-12-31 10:05:24 +0300

    changed test data

--- yii-application/frontend/views/site/_list.php
+++ yii-application/frontend/views/site/_list.php
@@ -6,6 +6,6 @@ use yii\helpers\HtmlPurifier;
 <div class="col-lg-4">
     <h2><?= Html::encode($model->title) ?></h2>    
     <p><?= HtmlPurifier::process($model->description) ?></p>
-    <p><?= HtmlPurifier::process($model->image) ?></p>
+    <p><div class="crop"><?= Html::img('@backendUploads/uploads/' . $model->image, ['class' => 'centered-and-cropped']) ?></div></p>
     <p><a class="btn btn-default" href="/index.php?r=post%2Fview&id=<?= HtmlPurifier::process($model->id) ?>">Show &raquo;</a></p>
 </div>
